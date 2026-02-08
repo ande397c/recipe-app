6bb66d04481605eb7be3116b0f418fe5bd8bbc46
anders iversen <ande397c@stud.kea.dk>
2026-02-08 19:41:42 +0100

    Update vite config with pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: false
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
